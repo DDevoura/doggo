0d7a87c2bcdc5222f3ac4c692ad6cf49911e6982
DDevoura <martekongslien@hotmail.com>
2019-12-09 17:45:20 +0100

    doggo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import paww from "./paww.png";
 
 class App extends Component {
   state = {
-    dogs: []
+    dogs: [],
+    currentDog: "",
+    currentImg: ""
   };
   componentDidMount() {
     axios.get("https://dog.ceo/api/breeds/list/all").then(res => {
@@ -14,20 +16,40 @@ class App extends Component {
     });
   }
 
+  getImages = doggo => {
+    axios.get(`https://dog.ceo/api/breed/${doggo}/images/random`).then(res => {
+      this.setState({
+        currentImg: res.data.message
+      });
+      return res.data.message;
+    });
+  };
+
+  handleChange = e => {
+    console.log(e.target.value);
+    this.setState({
+      currentDog: e.target.value,
+      currentImg: this.getImages(e.target.value)
+    });
+  };
+
   render() {
+    console.log(this.state.currentDog);
+    console.log(this.state.currentImg);
     return (
       <div className="App">
         <img className="paw" src={paww} />
         <h1 className="dick">shit doggo app</h1>
-        <select className="menu">
+        <p>
+          doggo is cute, doggo is great, doggo bark bark woof, <br /> cute doggo
+          wag tail, wag waggy bark woof dog.
+        </p>
+        <select className="menu" onChange={this.handleChange}>
           {this.state.dogs.map(dog => {
             return <option value={dog}>{dog}</option>;
           })}
         </select>
-        <p>
-          use this shit doggo app to look at cute doggos without actually seeing
-          any doggos.
-        </p>
+        <img className="dogimg" src={this.state.currentImg} />
       </div>
     );
   }
